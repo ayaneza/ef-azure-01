1d8f3dbbe487bbf6646e3c264a292aa166991f9e
Arturo Yañez <ayanezayal@gmail.com>
2024-10-26 16:53:03 -0500

    Update AuthController.java

--- src/main/java/com/proyecto/backend/controller/AuthController.java
+++ src/main/java/com/proyecto/backend/controller/AuthController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/autenticar")
+@CrossOrigin(origins = "https://proyectofrontend.azurewebsites.net/login/inicio") // Permitir solicitudes desde esta URL
 public class AuthController {
     @Autowired
     AutentificacionService autentificacionService;
@@ -23,34 +23,31 @@ public class AuthController {
     @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
         try {
-            Thread.sleep(Duration.ofSeconds(1));//tiempo en que va a devolver la respuesta
+            Thread.sleep(Duration.ofSeconds(1)); // Tiempo en que va a devolver la respuesta
             String[] datosUsuario = autentificacionService.validarUsuario(loginRequestDTO);
-            System.out.println("Respuesta backend : " + Arrays.toString(datosUsuario));
+            System.out.println("Respuesta backend: " + Arrays.toString(datosUsuario));
 
             if (datosUsuario == null) {
                 return new LoginResponseDTO("01", "Error: Problemas en la autenticación", "", "");
-
             } else {
                 return new LoginResponseDTO("00", "", datosUsuario[0], datosUsuario[1]);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return new LoginResponseDTO("99", "Error: Ocurrio un problema", "", "");
-
-
+            return new LoginResponseDTO("99", "Error: Ocurrió un problema", "", "");
         }
     }
+
     @GetMapping("/get-integrantes")
     public ResponseEntity<List<String[]>> listarUsuarios() {
         try {
-            List<String[]> usuarios =  autentificacionService.listarUsuarios();
-            return ResponseEntity.ok(usuarios);  // Devuelve la lista de usuarios como respuesta HTTP 200
+            List<String[]> usuarios = autentificacionService.listarUsuarios();
+            return ResponseEntity.ok(usuarios); // Devuelve la lista de usuarios como respuesta HTTP 200
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();  // Devuelve error 500 si hay un problema
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // Devuelve error 500 si hay un problema
         }
     }
 }
-
 
 
 
